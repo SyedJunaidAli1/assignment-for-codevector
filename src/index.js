7ac05de74f200cd6ab635182bdb76ebbf84b45ea
SyedJunaidAli1 <syedjunaidali790@gmail.com>
2026-06-22 13:15:15 +0530

    feat: env setup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import express from "express";
+import "dotenv/config";
 
 const app = express();
+const port = process.env.PORT || 8000;
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello World!" });
 });
 
-app.listen(8000, () => {
-  console.log("Server is running on port 8000");
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
