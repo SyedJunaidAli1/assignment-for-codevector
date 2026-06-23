be9b1ce4e4b9caffc0aa1599bd16f8c61abc604f
SyedJunaidAli1 <syedjunaidali790@gmail.com>
2026-06-23 15:19:38 +0530

    feat: cursor pagination added

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,12 +1,44 @@
 import Product from "../model/products.js";
 
+// offset pagination
+// export const getProducts = async (req, res) => {
+//   try {
+//     const query = {};
+//     if (req.query.category) {
+//       query.category = req.query.category;
+//     }
+//     let page = Number(req.query.page) || 1;
+//     let limit = Number(req.query.limit) || 10;
+//     let skip = (page - 1) * limit;
+//     const products = await Product.find(query).skip(skip).limit(limit);
+//     res.status(200).json(products);
+//   } catch (error) {
+//     res.status(500).json({ message: error.message });
+//   }
+// };
+
+//cursor pagination
 export const getProducts = async (req, res) => {
   try {
-    let page = Number(req.query.page) || 1;
+    const query = {};
+    if (req.query.category) {
+      query.category = req.query.category;
+    }
+
     let limit = Number(req.query.limit) || 10;
-    let skip = (page - 1) * limit;
-    const products = await Product.find().skip(skip).limit(limit);
-    res.status(200).json(products);
+
+    if (req.query.nextcursor) {
+      query._id = {
+        $lt: req.query.nextcursor,
+      };
+    }
+
+    const products = await Product.find(query).sort({ _id: -1 }).limit(limit);
+
+    const lastProduct = products[products.length - 1];
+    const nextcursor = lastProduct?._id;
+
+    res.status(200).json({ products, nextcursor });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
